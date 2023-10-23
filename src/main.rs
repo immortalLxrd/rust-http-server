be10e97ea283c1d7be8a953162766637242f335a
immortalLxrd <pavelpxxx@gmail.com>
2023-10-23 21:17:11 +0300

    Respond with content

--- src/main.rs
+++ src/main.rs
@@ -24,20 +24,85 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+struct ResponseHeaders<'a> {
+    content_type: &'a str,
+    content_length: usize,
+}
+
+struct ResponseMessage<'a> {
+    protocol: &'a str,
+    status_code: &'a str,
+    message: &'a str,
+    headers: Option<ResponseHeaders<'a>>,
+    body: Option<&'a str>,
+}
+
+impl<'a> ResponseMessage<'a> {
+    fn new(
+        protocol: &'a str,
+        status_code: &'a str,
+        message: &'a str,
+        headers: Option<ResponseHeaders<'a>>,
+        body: Option<&'a str>,
+    ) -> Self {
+        Self {
+            protocol,
+            status_code,
+            message,
+            headers,
+            body,
+        }
+    }
+
+    fn to_string(self) -> String {
+        let mut result = format!(
+            "{} {} {}\r\n",
+            self.protocol, self.status_code, self.message
+        );
+        if let (Some(headers), Some(body)) = (self.headers, self.body) {
+            result += &format!(
+                "Content-Type: {}\r\nContent-Length: {}\r\n\r\n{}",
+                headers.content_type, headers.content_length, body,
+            );
+        }
+        result
+    }
+}
+
 fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
     let mut buf: [u8; 128] = [0; 128];
     stream.read(&mut buf)?;
 
     let content = str::from_utf8(&buf)?;
     let content_splited: Vec<&str> = content.split(" ").collect();
-    let path = content_splited[1];
+    let path = content_splited[1].split("/").collect::<Vec<&str>>();
 
     match path {
-        "/" => {
-            stream.write(b"HTTP/1.1 200 OK\r\n\r\n")?;
+        path if path[1] == "" => {
+            let message = ResponseMessage::new("HTTP/1.1", "200", "OK", Option::None, Option::None)
+                .to_string();
+            stream.write(message.as_bytes())?;
         }
-        path => {
-            stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n")?;
+        path if path[1] == "echo" => {
+            let headers = ResponseHeaders {
+                content_type: "text/plain",
+                content_length: path[2].len(),
+            };
+            let message = ResponseMessage::new(
+                "HTTP/1.1",
+                "200",
+                "OK",
+                Option::Some(headers),
+                Option::Some(path[2]),
+            )
+            .to_string();
+            stream.write(message.as_bytes())?;
+        }
+        _ => {
+            let message =
+                ResponseMessage::new("HTTP/1.1", "404", "Not Found", Option::None, Option::None)
+                    .to_string();
+            stream.write(message.as_bytes())?;
         }
     }
 
