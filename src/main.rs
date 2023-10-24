47cecfdacc26ce896e4cf73bd10b2f78ddb7159e
immortalLxrd <pavelpxxx@gmail.com>
2023-10-24 13:03:48 +0300

    Refactoring. If else statement replaced by match

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,17 @@ struct ResponseHeaders<'a> {
     content_length: usize,
 }
 
+impl<'a> ResponseHeaders<'a> {
+    fn new(content_type: &'a str, content: &'a str) -> Self {
+        let content_length = content.len();
+
+        Self {
+            content_type,
+            content_length,
+        }
+    }
+}
+
 struct ResponseMessage<'a> {
     protocol: &'a str,
     status_code: &'a str,
@@ -60,6 +71,7 @@ impl<'a> ResponseMessage<'a> {
             self.protocol, self.status_code, self.message
         );
         if let (Some(headers), Some(body)) = (self.headers, self.body) {
+            let headers = ResponseHeaders::new(headers.content_type, body);
             result += &format!(
                 "Content-Type: {}\r\nContent-Length: {}\r\n\r\n{}",
                 headers.content_type, headers.content_length, body,
@@ -78,13 +90,20 @@ fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
     let content = str::from_utf8(&buf)?;
     let content_splited: Vec<&str> = content.split(' ').collect();
     let path = content_splited[1].strip_prefix('/').unwrap();
+    let (route, body) = if let Some((route, body)) = path.split_once('/') {
+        (route, body)
+    } else {
+        (path, "")
+    };
 
-    if let Some(body) = path.strip_prefix("echo/") {
-        if body.len() > 0 {
-            let headers = ResponseHeaders {
-                content_type: "text/plain",
-                content_length: body.len(),
-            };
+    match (route, body) {
+        (route, _) if route == "" => {
+            let message = ResponseMessage::new("HTTP/1.1", "200", "OK", Option::None, Option::None)
+                .to_string();
+            stream.write(message.as_bytes())?;
+        }
+        (route, body) if route == "echo" && body.len() > 0 => {
+            let headers = ResponseHeaders::new("text/plain", body);
             let message = ResponseMessage::new(
                 "HTTP/1.1",
                 "200",
@@ -94,22 +113,14 @@ fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
             )
             .to_string();
             stream.write(message.as_bytes())?;
-        } else {
+        }
+        _ => {
             let message =
                 ResponseMessage::new("HTTP/1.1", "404", "Not Found", Option::None, Option::None)
                     .to_string();
             stream.write(message.as_bytes())?;
         }
-    } else if path.len() == 0 {
-        let message =
-            ResponseMessage::new("HTTP/1.1", "200", "OK", Option::None, Option::None).to_string();
-        stream.write(message.as_bytes())?;
-    } else {
-        let message =
-            ResponseMessage::new("HTTP/1.1", "404", "Not Found", Option::None, Option::None)
-                .to_string();
-        stream.write(message.as_bytes())?;
-    };
+    }
 
     Ok(())
 }
