3d929ecee77d4d9d208ac1418c08b1c7914b1f3f
immortalLxrd <pavelpxxx@gmail.com>
2023-10-28 16:56:21 +0300

    Get a file. Fixed

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::{
     env,
     error::Error,
-    fs::File,
-    io::{self, Read, Write},
+    fs,
+    io::{Read, Write},
     net::{TcpListener, TcpStream},
     path::Path,
     str, thread,
@@ -11,21 +11,14 @@ use std::{
 mod response;
 use response::response::{ResponseHeaders, ResponseMessage};
 
-fn get_file_content(path: &Path) -> io::Result<String> {
-    let mut file = File::open(path)?;
-    let mut buf = String::new();
-    file.read_to_string(&mut buf)?;
-
-    Ok(buf)
-}
-
 fn handle_connection(mut stream: TcpStream, dir: Option<String>) -> Result<(), Box<dyn Error>> {
-    let mut buf: [u8; 128] = [0; 128];
+    let mut buf: [u8; 4096] = [0; 4096];
     stream.read(&mut buf)?;
 
     let content = str::from_utf8(&buf)?;
     let content_splited: Vec<&str> = content.split(' ').collect();
 
+    let method = content_splited[0];
     let path = content_splited[1].strip_prefix('/').unwrap();
     let (route, body) = if let Some((route, body)) = path.split_once('/') {
         (route, body)
@@ -37,27 +30,20 @@ fn handle_connection(mut stream: TcpStream, dir: Option<String>) -> Result<(), B
     let headers_splited: Vec<&str> = headers.split("\r\n").collect();
 
     let not_found =
-        &ResponseMessage::new("HTTP/1.1", "404", "Not Found", Option::None, Option::None)
-            .as_bytes();
+        ResponseMessage::new("HTTP/1.1", "404", "Not Found", Option::None, Option::None).as_bytes();
 
-    match route {
-        "" => {
-            let message =
-                &ResponseMessage::new("HTTP/1.1", "200", "OK", Option::None, Option::None)
-                    .as_bytes();
-            stream.write(message)?;
-        }
+    let response = match route {
+        "" => ResponseMessage::new("HTTP/1.1", "200", "OK", Option::None, Option::None).as_bytes(),
         "echo" if !body.is_empty() => {
             let headers = ResponseHeaders::new("text/plain", body);
-            let message = &ResponseMessage::new(
+            ResponseMessage::new(
                 "HTTP/1.1",
                 "200",
                 "OK",
                 Option::Some(headers),
                 Option::Some(body),
             )
-            .as_bytes();
-            stream.write(message)?;
+            .as_bytes()
         }
         "user-agent" => {
             let user_agent_pattern = "User-Agent: ";
@@ -68,15 +54,14 @@ fn handle_connection(mut stream: TcpStream, dir: Option<String>) -> Result<(), B
             let response_content = finded_header.strip_prefix(user_agent_pattern).unwrap();
 
             let headers = ResponseHeaders::new("text/plain", response_content);
-            let message = &ResponseMessage::new(
+            ResponseMessage::new(
                 "HTTP/1.1",
                 "200",
                 "OK",
                 Option::Some(headers),
                 Option::Some(response_content),
             )
-            .as_bytes();
-            stream.write(message)?;
+            .as_bytes()
         }
         "files" if !body.is_empty() => {
             if let Some(dir) = dir {
@@ -85,33 +70,43 @@ fn handle_connection(mut stream: TcpStream, dir: Option<String>) -> Result<(), B
                 } else {
                     &dir
                 };
-                let s = dir.to_owned() + &"/" + body;
+                let s = String::from(dir) + &"/" + body;
                 let path = Path::new(&s);
 
-                match get_file_content(path) {
-                    Ok(content) => {
-                        let headers = ResponseHeaders::new("application/octet-stream", &content);
-                        let message = &ResponseMessage::new(
-                            "HTTP/1.1",
-                            "200",
-                            "OK",
-                            Option::Some(headers),
-                            Option::Some(&content),
-                        )
-                        .as_bytes();
-                        stream.write(message)?;
-
-                        return Ok(());
+                match method {
+                    "GET" => {
+                        if path.is_file() {
+                            match fs::read_to_string(path) {
+                                Ok(content) => {
+                                    let headers =
+                                        ResponseHeaders::new("application/octet-stream", &content);
+                                    ResponseMessage::new(
+                                        "HTTP/1.1",
+                                        "200",
+                                        "OK",
+                                        Option::Some(headers),
+                                        Option::Some(&content),
+                                    )
+                                    .as_bytes()
+                                }
+                                Err(_) => not_found,
+                            }
+                        } else {
+                            not_found
+                        }
                     }
-                    Err(_) => (),
+                    "POST" => {
+                        not_found
+                    }
+                    _ => not_found,
                 }
+            } else {
+                not_found
             }
-            stream.write(not_found)?;
         }
-        _ => {
-            stream.write(not_found)?;
-        }
-    }
+        _ => not_found,
+    };
+    stream.write(&response)?;
 
     Ok(())
 }
